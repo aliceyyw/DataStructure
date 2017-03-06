5877627536db93f8c650423b7545e0fdba96e82d
wangyongyu <aliceyyw@foxmail.com>
2017-03-07 02:39:28 +0800

    update the mindepth method

--- BinaryTree.java
+++ BinaryTree.java
@@ -179,12 +179,8 @@ public class BinaryTree {
     public int minDepth(TreeNode root){
         if(root==null)
             return 0;
-        if(root.left==null&&root.right==null)
-            return 1;
-        if(root.left==null){
-            return minDepth(root.right)+1;
-        }if(root.right==null){
-            return minDepth(root.left)+1;
+        if(root.left==null||root.right==null){
+            return Math.max(minDepth(root.left),minDepth(root.right))+1;
         }
         return Math.min(minDepth(root.left),minDepth(root.right))+1;
     }
